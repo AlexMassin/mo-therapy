e0cfda0c18f7f878011076d6c27de09f103c4dd2
Alex Massin <aptmassin@gmail.com>
2025-10-02 01:13:48 -0400

    Update rotating text slogans: add limitless and performance

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -80,12 +80,12 @@ export default function Hero() {
                 <motion.span
                   className="pt-0.5 sm:pt-1 md:pt-2"
                   layout
-                  transition={{ type: 'spring', damping: 30, stiffness: 400 }}
+                  transition={{ type: 'spring', damping: 40, stiffness: 400 }}
                 >
                   Your M.O. is{' '}
                 </motion.span>
                 <RotatingText
-                  texts={['your hustle', 'pain free', 'in your control', 'here']}
+                  texts={['your hustle', 'pain free', 'limitless', 'performance', 'here']}
                   mainClassName="px-2 sm:px-2 md:px-3 bg-primary-300 text-black overflow-hidden py-1 sm:py-1 md:py-2 justify-center rounded-lg"
                   staggerFrom={'last'}
                   initial={{ y: '100%' }}
@@ -93,7 +93,7 @@ export default function Hero() {
                   exit={{ y: '-120%' }}
                   staggerDuration={0.025}
                   splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-                  transition={{ type: 'spring', damping: 30, stiffness: 400 }}
+                  transition={{ type: 'spring', damping: 40, stiffness: 400 }}
                   rotationInterval={2000}
                 />
               </motion.h1>
